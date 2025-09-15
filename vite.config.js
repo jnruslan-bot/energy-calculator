a1fbec859ffa7093a9fd96528a07c8f0d34240c5
Арслан <you@example.com>
2025-09-15 09:45:21 +0500

    fix vite base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/energy-calculator/',   // имя твоего репозитория
   plugins: [react()],
 })
